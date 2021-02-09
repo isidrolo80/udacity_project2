f33c056bb22a3780e0ab1c1e9ef85b7f34fecf19
Isidro Lopez <isidrolopez@Isidros-MacBook-Pro.local>
2021-02-09 15:24:39 -0800

    version 3

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -40,44 +40,32 @@ function initialLoad() {
 window.addEventListener("scroll", function() {  
 	const navVarOptions = document.querySelectorAll('.navOptions');
   	navVarOptions.forEach(tab => tab.addEventListener('scroll',changeOptionNavBar));
-  if (window.scrollY > currentSection[0].offsetTop && window.scrollY <= currentSection[1].offsetTop+10) {
-  	navVarOptions[0].classList.remove('active');
-	navVarOptions[1].classList.add('active');
-	navVarOptions[2].classList.remove('active');
-	navVarOptions[3].classList.remove('active');
-	navVarOptions[4].classList.remove('active');
-  } else if (window.scrollY > (currentSection[1].offsetTop) && window.scrollY <= currentSection[2].offsetTop ) {
-  	navVarOptions[0].classList.remove('active');
-	navVarOptions[1].classList.remove('active');
-	navVarOptions[2].classList.add('active');
-	navVarOptions[3].classList.remove('active');
-	navVarOptions[4].classList.remove('active');
-  } else if (window.scrollY >= currentSection[2].offsetTop && window.scrollY <= currentSection[3].offsetTop) {
-  	navVarOptions[0].classList.remove('active');
-	navVarOptions[1].classList.remove('active');
-	navVarOptions[2].classList.remove('active');
-	navVarOptions[3].classList.add('active');
-	navVarOptions[4].classList.remove('active');
-  } else if (window.scrollY >= currentSection[3].offsetTop) {
-  	navVarOptions[0].classList.remove('active');
-  	navVarOptions[1].classList.remove('active');
-	navVarOptions[2].classList.remove('active');
-	navVarOptions[3].classList.remove('active');
-	navVarOptions[4].classList.add('active');
-  } else {
-  	navVarOptions[0].classList.add('active');
-  	navVarOptions[1].classList.remove('active');
-	navVarOptions[2].classList.remove('active');
-	navVarOptions[3].classList.remove('active');
-	navVarOptions[4].classList.remove('active');
-  }
-  
+  	let index;
+  	navVarOptions.forEach(function(object, i){
+  		console.log(currentSection[i].offsetTop);	
+  		if (window.scrollY >= currentSection[i].offsetTop && window.scrollY < currentSection[i].offsetTop*1.1) {
+  			navVarOptions.forEach(tab => {
+	    	tab.classList.remove('active');
+			});
+			index = i; 
+		} else if (window.scrollY < currentSection[0].offsetTop) {
+			navVarOptions.forEach(tab => {
+	    	tab.classList.remove('active');
+			});
+			index = 0;
+		} 
+  	});
+  	
+  	navVarOptions[index].classList.add('active');
+
   function changeOptionNavBar(element) {
 	navVarOptions.forEach(tab => {
 	    tab.classList.remove('active');
 	});
 	element.currentTarget.classList.toggle('active');
 	}
+
+
 });
 
 
@@ -102,54 +90,27 @@ function toggleresponsive() {
 // build the nav
 function buildNav() {
 	const loadOptions = document.querySelector('#myTopnav');
+	let navElement = [document.createElement('li'), document.createElement('li'), document.createElement('li'), document.createElement('li'), document.createElement('li')];	
+	let link = [document.createElement('a'), document.createElement('a'), document.createElement('a'), document.createElement('a'), document.createElement('a')];
 
-	const navElement = document.createElement('li');
-	const link0 = document.createElement('a');
-	navElement.setAttribute('class', 'navOptions');
-	link0.textContent = "Home";
-	link0.setAttribute('href', "#home");
-	loadOptions.appendChild(navElement);
-	navElement.appendChild(link0);
+	navElement[0].setAttribute('class', 'navOptions');
+	navElement[0].textContent = "Home";
+	navElement[0].addEventListener('click', function goToScreen(){
+		window.location.hash = "home";
+	});
+	loadOptions.appendChild(navElement[0]);
 
-
-	const navElement1 = document.createElement('li');
-	const link1 = document.createElement('a');
-	navElement1.setAttribute('class', 'navOptions');
-	link1.textContent = "Section 1";
-	link1.setAttribute('href', "#section1");
-	loadOptions.appendChild(navElement1);
-	navElement1.appendChild(link1);
-
-	const navElement2 = document.createElement('li');
-	const link2 = document.createElement('a');
-	navElement2.setAttribute('class', 'navOptions');
-	link2.textContent = "Section 2";
-	link2.setAttribute('href', "#section2");
-	loadOptions.appendChild(navElement2);
-	navElement2.appendChild(link2);
-
-	const navElement3 = document.createElement('li');
-	const link3 = document.createElement('a');
-	navElement3.setAttribute('class', 'navOptions');
-	link3.textContent = "Section 3";
-	link3.setAttribute('href', "#section3");
-	loadOptions.appendChild(navElement3);
-	navElement3.appendChild(link3);
-
-	const navElement4 = document.createElement('li');
-	const link4 = document.createElement('a');
-	navElement4.setAttribute('class', 'navOptions');
-	link4.textContent = "Section 4";
-	link4.setAttribute('href', "#section4");
-	loadOptions.appendChild(navElement4);
-	navElement4.appendChild(link4);
-
-
-
+	for (let numberOfElements = 1; numberOfElements < 5; numberOfElements++) {
+		navElement[numberOfElements].setAttribute('class', 'navOptions');
+		navElement[numberOfElements].textContent = "Section " + numberOfElements;
+		navElement[numberOfElements].addEventListener('click', function goToScreen(){
+			window.location.hash = "section"+ numberOfElements;
+			});
+		loadOptions.appendChild(navElement[numberOfElements]);
+	}
 	//let initialOptionsHTML = '<a href="#home" class="navOptions active">Home</a> <a href="#section1" class="navOptions">Section 1</a><a href="#section2" class="navOptions">Section 2</a> <a href="#section3" class="navOptions">Section 3</a><a href="#section4" class="navOptions">Section 4</a>';
 	//loadOptions.insertAdjacentHTML('afterbegin', initialOptionsHTML);
 }
-
 // Add class 'active' to section when near top of viewport
 
 
